2ad21bdfb2778fdaf5ba737039c649bd533771f1
Asad Aslam <opworrier@gmail.com>
2024-11-05 12:09:20 +0500

    updation

--- appwritebasic/src/appwrite/auth.js
+++ appwritebasic/src/appwrite/auth.js
@@ -12,6 +12,46 @@ export class AuthService {
         .setProject(conf.appwriteProjectId);
         this.account = new Account(this.client);
     }
+    async createAccount({email, password, name}){
+        try{
+            await this.Client.account.create(ID.unique(), email,password,name);
+            if(userAccount){
+                return this.login((email, password));
+            }else {
+                return userAccount;
+            }
+
+        }catch (error){
+            throw error;
+        }
+    }
+
+    async login({email, password}) {
+        try{
+            return await this.account.createSession(email, password);
+
+        }catch(error){
+            throw error; 
+        }
+        
+    }
+
+    async getCurrentUser (){
+        try {
+            return await this.account.get();
+        }catch(error){
+            console.log("Appwrite service :: getCurrentUser :: error", error)
+        }
+        return null;
+    }
+
+    async logout (){
+        try {
+            return await this.account.deleteSession();
+        } catch (error) {
+            console.log("Appwrite service :: getCurrentUser :: error", error);
+        }
+    }
 }
 
 const authService = new AuthService ();
