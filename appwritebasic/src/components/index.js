8cdf7709400fa75a8f9536b9bf2df2f7f2f7aea6
Asad Aslam <opworrier@gmail.com>
2024-11-20 14:29:29 +0500

    updation

--- appwritebasic/src/components/index.js
+++ appwritebasic/src/components/index.js
@@ -1,13 +1,15 @@
-import Header from "./header/header"
-import Footer from "./footer/footer"
-import Container from "./container/Container"
-import logo from "./logo"
-import LogoutBtn from "./header/LogoutBtn"
+import Header from "./header/header";
+import Footer from "./footer/footer";
+import Container from "./container/Container";
+import logo from "./logo";
+import LogoutBtn from "./header/LogoutBtn";
+import RTE from "./RTE";
 
 export {
     Header,
     Footer,
     Container,
     logo,
-    LogoutBtn
+    LogoutBtn,
+    RTE
 }
