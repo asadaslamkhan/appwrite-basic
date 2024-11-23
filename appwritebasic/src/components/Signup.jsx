a00014f4bb93da2f2adb6d513f7e000754bfd49c
Asad Aslam <opworrier@gmail.com>
2024-11-23 13:09:41 +0500

    modification

--- appwritebasic/src/components/Signup.jsx
+++ appwritebasic/src/components/Signup.jsx
@@ -34,7 +34,7 @@
                 </div>
                 <h2 className="text-center text-2xl font-bold leading-tight">Sign up to create account</h2>
                 <p className="mt-2 text-center text-base text-black/60">
-                    Already have an account?&nbsp;
+                    Already have an Account?&nbsp;
                     <Link
                         to="/login"
                         className="font-medium text-primary transition-all duration-200 hover:underline"
